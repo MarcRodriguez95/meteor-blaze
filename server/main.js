e6864826b8f1788e2bc52b11a0172fad0828b643
Marc <marc.rodriguez451@gmail.com>
2017-01-25 17:40:03 +0100

    añadiendo meteor

---
+++ server/main.js
@@ -0,0 +1,6 @@
+import { Meteor } from 'meteor/meteor';
+import '../imports/api/tasks.js';
+/**Importamos las llamadas que cambian la base de datos**/
+Meteor.startup(() => {
+  // code to run on server at startup
+});
